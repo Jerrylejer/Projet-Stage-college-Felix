6eed55baf921906eb1ab8dbfd9c1042c59542284
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2022-10-19 18:01:45 +0200

    ajout photos projets

--- portfolio_felix_richard/src/Components/Activites/Activites.js
+++ portfolio_felix_richard/src/Components/Activites/Activites.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import MaisonForêt1 from '../../assets/Atelier_2019_Maison_Autonome_forêt(1).jpg';
+import MaisonForêt2 from '../../assets/Atelier_2019_Maison_Autonome_forêt(2).jpg';
+import MaisonForêt3 from '../../assets/Atelier_2019_Maison_Autonome_forêt(3).jpg';
+import PlaceVide1 from '../../assets/Atelier_2021_Décors_PlaceVide(1).jpg';
+import PlaceVide2 from '../../assets/Atelier_2021_Décors_PlaceVide(2).jpg';
+import PlaceVide3 from '../../assets/Atelier_2021_Décors_PlaceVide(3).jpg';
+import PlaceVide4 from '../../assets/Atelier_2021_Décors_PlaceVide(4).jpg';
+import LumièreObjets1 from '../../assets/Atelier_2021_Lumière_Objets(1).jpg';
+import LumièreObjets2 from '../../assets/Atelier_2021_Lumière_Objets(2).jpg';
+import LumièreObjets3 from '../../assets/Atelier_2021_Lumière_Objets(3).jpg';
+import LumièreObjets4 from '../../assets/Atelier_2021_Lumière_Objets(4).jpg';
 import ensemble from '../../assets/ensemble.jpg';
-import ensembleFront from '../../assets/ensembleFront.jpg';
-import ensembleFond from '../../assets/ensembleFond.jpg';
-
 
 const Curriculum = () => {
     return (
         <div>
-            <div className='text-center'>            
+            <div className='text-center'>
                 <p className='mb-4'>
                     Depuis 2019 : Adhérent au CAUE et élève aux cours et stages
                     d'architecture
@@ -16,7 +24,8 @@ const Curriculum = () => {
                     Juin 2022 : Présentation de nos travaux devant les parents
                     d'élèves (casque audio conseillé !)
                 </p>
-                <iframe className='max-w-[350px] md:max-w-[744px] mx-auto'
+                <iframe
+                    className='max-w-[350px] md:max-w-[744px] mx-auto'
                     src='https://www.veed.io/embed/5fc6425e-fec2-4a8d-9347-9f952c3e0ffe'
                     width='744'
                     height='504'
@@ -26,26 +35,107 @@ const Curriculum = () => {
                     mozallowfullscreen
                     allowfullscreen
                 ></iframe>
+                <br />
+                <hr />
                 <p className='mb-4 mt-8'>
-                    Mes maquettes réalisées au cours de l'année 2022
+                    Maquette réalisée au cours de l'année 2019 sur le thème "maison autonome au coeur de la forêt".
+                </p>
+                <hr />
+                <br />
+                <p className='mb-8'>
+                    <img
+                        src={MaisonForêt1}
+                        alt='ensemble-maquettes'
+                        className='lg:max-w-[800px] mx-auto'
+                    />
+                </p>
+                <p className='mb-8'>
+                    <img
+                        src={MaisonForêt2}
+                        alt='ensemble-maquettes'
+                        className='lg:max-w-[800px] mx-auto'
+                    />
+                </p>
+                <p className='mb-8'>
+                    <img
+                        src={MaisonForêt3}
+                        alt='ensemble-maquettes'
+                        className='lg:max-w-[800px] mx-auto'
+                    />
+                </p>
+                <br />
+                <hr />
+                <p className='mb-4 mt-8'>
+                    Maquette réalisée au cours de l'année 2021 sur le thème "La lumière sur les objets".
+                </p>
+                <hr />
+                <br />
+                <p className='mb-8'>
+                    <img
+                        src={LumièreObjets1}
+                        alt='ensemble-maquettes'
+                        className='lg:max-w-[800px] mx-auto'
+                    />
+                </p>
+                <p className='mb-8'>
+                    <img
+                        src={LumièreObjets2}
+                        alt='ensemble-maquettes'
+                        className='lg:max-w-[800px] mx-auto'
+                    />
+                </p>
+                <p className='mb-8'>
+                    <img
+                        src={LumièreObjets3}
+                        alt='ensemble-maquettes'
+                        className='lg:max-w-[800px] mx-auto'
+                    />
+                </p>
+                <p className='mb-8'>
+                    <img
+                        src={LumièreObjets4}
+                        alt='ensemble-maquettes'
+                        className='lg:max-w-[800px] mx-auto'
+                    />
+                </p>
+                <br />
+                <hr />
+                <p className='mb-4 mt-8'>
+                    Maquette réalisée au cours de l'année 2021 sur le thème "Décorer une place vide".
+                </p>
+                <hr />
+                <br />
+                <p className='mb-8'>
+                    <img
+                        src={PlaceVide1}
+                        alt='ensemble-maquettes'
+                        className='lg:max-w-[800px] mx-auto'
+                    />
+                </p>
+                <p className='mb-8'>
+                    <img
+                        src={PlaceVide2}
+                        alt='ensemble-maquettes'
+                        className='lg:max-w-[800px] mx-auto'
+                    />
+                </p>
+                <p className='mb-8'>
+                    <img
+                        src={PlaceVide3}
+                        alt='ensemble-maquettes'
+                        className='lg:max-w-[800px] mx-auto'
+                    />
+                </p>
+                <p className='mb-8'>
+                    <img
+                        src={PlaceVide4}
+                        alt='ensemble-maquettes'
+                        className='lg:max-w-[800px] mx-auto'
+                    />
                 </p>
                 <p className='mb-8'>
                     <img
                         src={ensemble}
-                        alt='ensemble-maquettes'
-                        className='lg:max-w-[800px] mx-auto'
-                    />
-                </p>
-                <p className='mb-8'>
-                    <img
-                        src={ensembleFront}
-                        alt='ensemble-maquettes'
-                        className='lg:max-w-[800px] mx-auto'
-                    />
-                </p>
-                <p className='mb-8'>
-                    <img
-                        src={ensembleFond}
                         alt='ensemble-maquettes'
                         className='lg:max-w-[800px] mx-auto'
                     />
--- portfolio_felix_richard/src/Components/Nav/Nav.js
+++ portfolio_felix_richard/src/Components/Nav/Nav.js
@@ -14,6 +14,7 @@ const Nav = () => {
     return (
         <>
             <nav className='relative list-none mt-4 mb-4 md:flex md:w-full lg:mt-[30px]'>
+                {/* NAV md et lg */}
                 <ul className='hidden text-blue-400 text-xl font-semibold text-center p-4 md:flex md:w-full md:justify-around lg:text-2xl lg:font-bold'>
                     <li className='pb-12 hover:text-blue-300 hover:underline'>
                         <Link to='/'>Présentation</Link>
@@ -25,6 +26,7 @@ const Nav = () => {
                         <Link to='/contact'>Contact</Link>
                     </li>
                 </ul>
+                {/* icônes et switch */}
                 <div onClick={toggleSwitch} className='block md:hidden'>
                     {nav ? (
                         <RiCloseCircleLine size={30} />
@@ -32,8 +34,9 @@ const Nav = () => {
                         <CgMenuRound size={30} />
                     )}
                 </div>
-
-                <div onClick={toggleSwitch}
+                {/* NAV sm et apparition/disparition */}
+                <div
+                    onClick={toggleSwitch}
                     className={
                         nav
                             ? 'fixed left-0 top-0 w-[100%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-1000'
